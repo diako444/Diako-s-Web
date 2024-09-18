31114bb657c364e8b3c1141baa4f02a48b7e2ab2
diako444 <65386351+diako444@users.noreply.github.com>
2024-09-18 17:26:53 +0330

    added a header and some images

--- Dia-first/src/App.jsx
+++ Dia-first/src/App.jsx
@@ -1,6 +1,15 @@
+
 function App() {
   return (
-    <h1>my first websiiite</h1>
+      <div className='navcontainer'>
+          <div><img src="../img/1.jpeg" alt="hi" /></div>
+          <div className="linkcontainer">
+            <a href="">Home</a>
+            <a href="">About Us</a>
+            <a href="">Contact</a>
+            <a href="">Products</a>
+          </div>
+      </div>
   )
 }
 
